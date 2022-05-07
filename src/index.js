2a732018145f53b5f9144b83a85d24d0d40a6e15
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-05-07 20:40:16 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+const data = [
+  {text: 'ja'}
+];
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+// defining an endpoint to return all ads
+app.get('/', (req, res) => {
+  res.send(data);
+});
+
+// starting the server
+app.listen(3001, () => {
+  console.log('listening on port 3001');
+});
